073cbf3eeaec1a246e7d1b88abc4d4ee42146de6
Your Name <akbarhusainkadiwala32@gmail.com>
2024-03-30 13:12:42 +0530

    updated jokes part

--- Backend/server.js
+++ Backend/server.js
@@ -8,12 +8,32 @@ app.get('/', (req, res) => {
 
 app.get('/api/jokes', (req, res) => {
     const jokes = [
-        { id: 1, title: "Why don't scientists trust atoms?", content: "Because they make up everything!" },
-        { id: 2, title: "What did one ocean say to the other ocean?", content: "Nothing, they just waved." },
-        { id: 3, title: "I'm reading a book on anti-gravity...", content: "It's impossible to put down!" },
-        { id: 4, title: "Parallel lines have so much in common.", content: "It's a shame they'll never meet." },
-        { id: 5, title: "Who did the scarecrow win an award?", content: "Because he was outstanding in his field!" }
-    ];
+        {
+            "id": 1,
+            "title": "Why don't scientists trust atoms?",
+            "content": "Because they make up everything!"
+        },
+        {
+            "id": 2,
+            "title": "What did one ocean say to the other ocean?",
+            "content": "Nothing, they just waved."
+        },
+        {
+            "id": 3,
+            "title": "I'm reading a book on anti-gravity...",
+            "content": "It's impossible to put down!"
+        },
+        {
+            "id": 4,
+            "title": "Parallel lines have so much in common.",
+            "content": "It's a shame they'll never meet."
+        },
+        {
+            "id": 5,
+            "title": "Who did the scarecrow win an award?",
+            "content": "Because he was outstanding in his field!"
+        }
+    ]
     res.send(jokes);
 })
 
